6526d8c7e3b670e314dc21b05c800f791e74240e
Pranay Sharma <sharmapranay1210@gmail.com>
2026-06-19 22:54:47 +0530

    Create Find the highest altitude .cpp

---
+++ Find the highest altitude .cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int curr = 0 ;
+        int maxi = 0;
+        int n = gain.size();
+        for(int i = 0 ; i < n ; i++){
+            curr += gain[i];
+            maxi = max(maxi , curr);
+        }
+        return maxi;
+    }
+};
